1f7036590b9e5b3a32129a6d27cab12ece69d59f
jpt0927 <jpt020927@gmail.com>
2025-07-13 09:21:54 +0000

    test

--- server.js
+++ server.js
@@ -16,22 +16,47 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+const sessionMiddleware = session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'lax'
+  }
+});
+
 const PORT = 3000;
 const HOST = '127.0.0.1';
 
 const path = require('path');
 const authRoutes = require('./server/routes/authRoutes');
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true
-}));
+// app.use(session({
+//   secret: process.env.SESSION_SECRET,
+//   resave: false,
+//   saveUninitialized: true
+// }));
 
+app.use(sessionMiddleware);
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, none) => {
+  const user = { id: id, name: '사용자' + id };
+  done(null, user);
+});
+
 passportConfig(passport);
+
+io.use((socket, next) => {
+  sessionMiddleware(socket.request, {}, next);
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/auth', authRoutes);
@@ -57,6 +82,9 @@ app.get('/lobby.html', (req, res) => {
 app.get('/profile.html', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'html', 'profile.html'));
 });
+app.get('/ranking.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'html', 'ranking.html'));
+});
 
 // 게임 맵 관리(숫자가 양수면 사과 존재, 숫자가 0이면 사과 없음)
 
@@ -64,7 +92,17 @@ app.get('/profile.html', (req, res) => {
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
-  console.log(`✅ A user connected: ${socket.id}`); // 유저 접속 시 콘솔에 메시지 출력
+  const session = socket.request.session;
+
+  if (!session.passport || !session.passport.user) {
+    console.log('미인증 사용자 접속 시도');
+    return socket.disconnect(true);
+  }
+
+  const userId = session.passport.user;
+  console.log(`✅ A user connected: ${userId}`); // 유저 접속 시 콘솔에 메시지 출력
+
+  socket.userId = userId;
 
   roomHandler.registerRoomsHandlers(io, socket, redisClient);
   socket.on('disconnecting', () => {
