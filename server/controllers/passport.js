2f7725aa8ecbcf6b66376ae24079792831c5e825
jpt0927 <jpt020927@gmail.com>
2025-07-13 05:47:06 +0000

    구글 로그인 오류 수정

--- server/controllers/passport.js
+++ server/controllers/passport.js
@@ -6,7 +6,7 @@ module.exports = (passport) => {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: 'http://www.applegame.shop/auth/google/callback'
+        callbackURL: 'https://www.applegame.shop/auth/google/callback'
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
